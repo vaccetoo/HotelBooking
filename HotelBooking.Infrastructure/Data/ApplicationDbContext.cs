b34526ee73100775fc30f2f461b02461f1d1cf6c
Vasil Delchev <vasildelchev97@gmail.com>
2026-02-08 18:14:45 +0200

    arrange solution nuget packages, layers and project references

---
+++ HotelBooking.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using HotelBooking.Infrastructure.Data.Extensions;
+using HotelBooking.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelBooking.Infrastructure.Data
+{
+	public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			builder.ApplySoftDeleteQueryFilters();
+
+			base.OnModelCreating(builder);
+		}
+	}
+}
---
+++ HotelBooking.Web/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using HotelBooking.Infrastructure.Data;
+using HotelBooking.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration configuration)
+		{
+			var connectionString = configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(connectionString));
+
+			services.AddDatabaseDeveloperPageExceptionFilter();
+
+			return services;
+		}
+
+		public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration configuration)
+		{
+			services.AddIdentity<ApplicationUser, IdentityRole<Guid>>(options => 
+			{ 
+				options.SignIn.RequireConfirmedAccount = true; 
+			})
+			.AddEntityFrameworkStores<ApplicationDbContext>()
+			.AddDefaultTokenProviders();
+
+			return services;
+		}
+
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+		{
+			return services;
+		}
+	}
+}
