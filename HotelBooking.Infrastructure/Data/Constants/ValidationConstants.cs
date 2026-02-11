15ff200ff62e001f0461aabd986b5d01867c06e6
Vasil Delchev <vasildelchev97@gmail.com>
2026-02-11 22:19:28 +0200

    add validation constants

---
+++ HotelBooking.Infrastructure/Data/Constants/ValidationConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace HotelBooking.Infrastructure.Data.Constants
+{
+	public static class ValidationConstants
+	{
+		public const int RoomNumberMaxLength = 5;
+
+		public const int RoomCapacityMinValue = 1;
+		public const int RoomCapacityMaxValue = 4;
+
+		public const int RatingMinValue = 1;
+		public const int RatingMaxValue = 5;
+
+		public const int CommentMinLength = 0;
+		public const int CommentMaxLength = 525;
+	}
+}
