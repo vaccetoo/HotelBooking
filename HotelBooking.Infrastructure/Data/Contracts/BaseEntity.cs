ecb3b3918672afab328d46c80b6bb02eacc75a67
Vasil Delchev <vasildelchev97@gmail.com>
2026-02-11 22:19:55 +0200

    add BaseEntity comments

--- HotelBooking.Infrastructure/Data/Contracts/BaseEntity.cs
+++ HotelBooking.Infrastructure/Data/Contracts/BaseEntity.cs
@@ -1,10 +1,20 @@
 ﻿
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
 namespace HotelBooking.Infrastructure.Data.Contracts
 {
 	public abstract class BaseEntity : ISoftDeletable
 	{
+		[Comment("Unique identifier")]
+		[Key]
 		public Guid Id { get; set; }
+
+		[Comment("Indicates whether the entity has been deleted or not")]
+		[Required]
 		public bool IsDeleted { get; set; }
+
+		[Comment("Shows the exact date and time of deletion - optional")]
 		public DateTime? DeletedOn { get; set; }
 	}
 }
